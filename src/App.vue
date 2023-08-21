e4d9cc59fcbd2244f431b3743cbfb29197c81980
karyna.kananovich <karyna4k.dev@gmail.com>
2023-08-21 19:08:12 +0400

    feat: update app file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
-<script setup></script>
-
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
-  <router-view />
+  <app-navigation />
+  <div class="container">
+    <router-view v-slot="{ Component }">
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <component
+          :is="Component"
+          :key="route.path"
+        ></component>
+      </transition>
+    </router-view>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import AppNavigation from '@/components/AppNavigation.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
